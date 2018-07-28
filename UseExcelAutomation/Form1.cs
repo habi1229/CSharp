8e032c67b7dbfbb455927d8053d7993190593f26
habi1229 <habi1229@gmail.com>
2018-07-28 14:44:11 +0900

    Excel save, read

--- UseExcelAutomation/Form1.cs
+++ UseExcelAutomation/Form1.cs
@@ -10,18 +10,69 @@ using System.Windows.Forms;
 
 using System.Runtime.InteropServices;
 using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
 
 namespace UseExcelAutomation
 {
     public partial class Form1 : Form
     {
+        string savFileName;
+
         public Form1()
         {
             InitializeComponent();
 
+            savFileName = Application.StartupPath + @"\test.xlsx";
         }
 
-        private static void WriteExcelData()
+        private void CreateExcelData()
+        {
+            // 신규 파일 생성시
+            List<string> testData = new List<string>()
+            { "Excel", "Access", "Word", "OneNote" };
+
+            Excel.Application excelApp = null;
+            Excel.Workbook wb = null;
+            Excel.Worksheet ws = null;
+
+            try
+            {
+                excelApp = new Excel.Application();
+
+                wb = excelApp.Workbooks.Add();
+
+                // 첫번째 Worksheet
+                ws = wb.Worksheets.get_Item(1) as Excel.Worksheet;
+
+                // 데이타 넣기
+                int r = 1;
+                foreach (var d in testData)
+                {
+                    ws.Cells[r, 1] = d;
+                    r++;
+                }
+
+                // 엑셀파일 저장
+                if (File.Exists(savFileName))
+                {
+                    File.Delete(savFileName);
+                }
+
+                wb.SaveAs(savFileName);
+
+                wb.Close(true);
+                excelApp.Quit();
+            }
+            finally
+            {
+                // Clean up
+                ReleaseExcelObject(ws);
+                ReleaseExcelObject(wb);
+                ReleaseExcelObject(excelApp);
+            }
+        }
+
+        public void ReadExcelData()
         {
             Excel.Application excelApp = null;
             Excel.Workbook wb = null;
@@ -31,32 +82,43 @@ namespace UseExcelAutomation
             {
                 excelApp = new Excel.Application();
 
-                // 엑셀 파일을 엽니다.
-                wb = excelApp.Workbooks.Open(@".\test.xlsx");
+                // 엑셀 파일 열기
+                wb = excelApp.Workbooks.Open(savFileName);
 
-                // 첫번째 Worksheet를 선택합니다.
+                // 첫번째 Worksheet
                 ws = wb.Worksheets.get_Item(1) as Excel.Worksheet;
 
-                // 해당 Worksheet에서 저장할 범위를 정합니다.
-                // 지금은 저장할 행렬의 크기만큼 지정합니다.
-                // 예시 Excel.Range rng = ws.Range["B2", "G8"];
-                int row = 0;
-                int column = 0;
-                Excel.Range rng = ws.Range[ws.Cells[1, 1], ws.Cells[row, column]];
-            }
-            catch (Exception ex)
-            {
-                throw ex;
+                // 현재 Worksheet에서 사용된 Range 전체를 선택
+                Excel.Range rng = ws.UsedRange;
+
+                // 현재 Worksheet에서 일부 범위만 선택
+                // Excel.Range rng = ws.Range[ws.Cells[2, 1], ws.Cells[5, 3]];
+
+                // Range 데이타를 배열 (One-based array)로
+                object[,] data = rng.Value;
+
+                for (int r = 1; r <= data.GetLength(0); r++)
+                {
+                    for (int c = 1; c <= data.GetLength(1); c++)
+                    {
+                        //Debug.Write(data[r, c].ToString() + " ");
+                    }
+                    //Debug.WriteLine("");
+                }
+
+                wb.Close(true);
+                excelApp.Quit();
             }
             finally
             {
+                // Clean up
                 ReleaseExcelObject(ws);
                 ReleaseExcelObject(wb);
                 ReleaseExcelObject(excelApp);
             }
         }
 
-        private static void ReleaseExcelObject(object obj)
+        private void ReleaseExcelObject(object obj)
         {
             try
             {
@@ -76,6 +138,11 @@ namespace UseExcelAutomation
             {
                 GC.Collect();
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CreateExcelData();
         }
     }
 }
